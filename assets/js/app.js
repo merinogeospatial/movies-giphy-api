2918186a19b22f92694e83d31810a9f06a6ffcb9
R <merinogeospatial@gmail.com>
2018-05-25 22:46:39 -0400

    gifs added and formatted properly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
+// Declare global variables
 let movArr = ["The Avengers","Home Alone", "Deadpool","Star Wars","Titanic","The Hunger Games","Finding Nemo",];
 let movie;
 
+// Create buttons from movie array
 function initButtons(arr) {
 
     for (let i = 0; i < arr.length; i++) {
@@ -10,10 +12,13 @@ function initButtons(arr) {
             .attr('data', arr[i])
             .attr('class', 'btn btn-secondary m-1 movie-button');
         $('#button-container').append(newButton);
+
     }
 }
 
+// On submit, push user input to movie array and reinit buttons
 $('#add-movie').on('click', function(){
+
     $('#button-container').empty();
     newMovie = $('#inlineFormInput').val().trim();
     movArr.push(newMovie);
@@ -21,18 +26,21 @@ $('#add-movie').on('click', function(){
 
 })
 
-initButtons(movArr);
-
-// Prevent enter from refreshing page
+// Prevent enter key event from refreshing page
 $('form').keypress(function(event) { 
+
     return event.keyCode != 13;
+
 }); 
 
+// Click listener on movie buttons, requests from giphy and omdb
 $('#button-container').on('click','button',function() {
+
     $('#loading-1').show();
     $('#loading-2').show();
 
     $('#movie-overview').empty();
+    $('#gif-container').empty();
 
     // OMDB API Request + Response Handling Here
     movie = $(this).attr('data').trim();
@@ -40,14 +48,18 @@ $('#button-container').on('click','button',function() {
     apiKey = 'trilogy';
     queryURL = baseURL + apiKey + '&t=' + movie;
     
-
+// OMDb API
     $.ajax({
+
         url: queryURL,
         method: 'GET',
         success:function(response){
+
             $('#loading-1').hide();
+
         } 
     }).then(function(response) {
+
         console.log(response);
 
         h1 = $('<h1>');
@@ -73,28 +85,46 @@ $('#button-container').on('click','button',function() {
     GIFapiKey = '&limit=10&api_key=l7BH2odGrHlcmcFxkXssYzBU6RXc94UM';
     GIFqueryURL = GIFbaseURL + movie + GIFapiKey;
 
+    // Giphy API
     $.ajax({
+
         url: GIFqueryURL,
         method: 'GET',
         success:function(response){
+
             $('#loading-2').hide();
+
         }
     }).then(function(data) {
+
         console.log(data);
-
-
 
         for (let i = 0; i < data.data.length; i++) {
             img = $('<img>');
-            img.attr('src', data.data[i].images.fixed_height_still.url)
+            img.attr('src', data.data[i].images.fixed_width_still.url)
                .attr('state','still')
-               .attr('still', data.data[i].images.fixed_height_still.url)
-               .attr('animate', data.data[i].images.fixed_height.url);
+               .attr('still', data.data[i].images.fixed_width_still.url)
+               .attr('animate', data.data[i].images.fixed_height.url)
+               .addClass('m-1');
             console.log(img);
             $('#gif-container').append(img);
+
         }
     });
 })
+
+// Click listener to toggle between animated gif and still image
+$('#gif-container').on('click', 'img', function() {
+    
+
+})
+
+
+
+
+
+
+initButtons(movArr);
 
 // Get giphy api
     // append to 10 gifs to div
@@ -104,6 +134,8 @@ $('#button-container').on('click','button',function() {
     // start with <img src="..." state="still" still="...jpg" animate="...gif"
     // create on click for the dynamic images
         // you will have to use the on click to pass binding from parent to child
+
+// Error handling if movie is not found
 
 
 
